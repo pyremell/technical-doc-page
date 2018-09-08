6036dd6a7d07ce691df7ef4ffd34b5502e340de8
Jacob Schaefer <jacobschaefer12@gmail.com>
2018-09-07 17:00:50 -0700

    Second commit

---
+++ app/assets/scripts/modules/sampleScript.js
@@ -0,0 +1,1 @@
+// Use multiple module files for each set of Javascript functionality, i.e. a file for a mobile menu and a file for an image carousel. Use imports to access functions from other files.
